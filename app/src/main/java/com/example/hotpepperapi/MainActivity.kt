29701be5a69b8b49afb399f3a8048e2996dc5766
hirotomo-yamazaki <hirotomo.yamazaki@gmail.com>
2022-12-02 16:32:49 +0900

    位置情報の権限がない場合にダイアログ表示

--- app/src/main/java/com/example/hotpepperapi/MainActivity.kt
+++ app/src/main/java/com/example/hotpepperapi/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.hotpepperapi
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.location.Location
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.example.hotpepperapi.viewModel.ViewModel
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
@@ -37,7 +43,28 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             .addOnFailureListener {
-                Toast.makeText(this, "位置情報をONにして下さい。", Toast.LENGTH_LONG).show()
+                showDialog()
             }
+    }
+
+    private fun showDialog(){
+        AlertDialog.Builder(this)
+            .setMessage(R.string.dialogMain)
+            .setPositiveButton("OK") { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    //Uriでどの設定を開くべきかを指定
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            //if user pushes negative button
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 }
